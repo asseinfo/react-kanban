1ce4660bd0693a82bc7d9b613151d9c3f0f54e7e
Leandro Lourenci <leandro@lourenci.com>
2019-04-05 08:34:35 -0300

    Setup Cypress

---
+++ cypress/integration/board.spec.js
@@ -0,0 +1,5 @@
+describe('Board Page', function () {
+  it('opens the board', function () {
+    cy.contains('Card')
+  })
+})
--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   collectCoverage: true,
   collectCoverageFrom: ['src/**'],
   coverageDirectory: 'coverage',
+  testMatch: ['<rootDir>/src/**/*.spec.js'],
   setupFilesAfterEnv: [
     'react-testing-library/cleanup-after-each',
     'jest-dom/extend-expect'
