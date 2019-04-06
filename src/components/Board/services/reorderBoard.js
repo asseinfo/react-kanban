1a3154d3f5120a601b173102bb1d31c907052482
Leandro Lourenci <leandro@lourenci.com>
2019-04-06 11:47:05 -0300

    More refactor

--- src/components/Board/services/reorderBoard.js
+++ src/components/Board/services/reorderBoard.js
@@ -1,5 +1,13 @@
 import { removeFromArrayAtPosition, addInArrayAtPosition, changeElementOfPositionInArray } from '@services/utils'
 
+function reorderCardsOnLane (lane, reorderCards) {
+  return { ...lane, cards: reorderCards(lane.cards) }
+}
+
+function reorderLanesOnBoard (board, reorderLanesMapper) {
+  return { ...board, lanes: board.lanes.map(reorderLanesMapper) }
+}
+
 function reorderBoard (board, source, destination) {
   var reorderedBoard
 
@@ -7,19 +15,22 @@ function reorderBoard (board, source, destination) {
   const destinationLane = board.lanes.find(lane => lane.id === destination.laneId)
 
   if (sourceLane.id === destinationLane.id) {
-    const reorderedCards = changeElementOfPositionInArray(sourceLane.cards, source.index, destination.index)
-    const reorderedLane = { ...sourceLane, cards: reorderedCards }
-    reorderedBoard = { ...board, lanes: board.lanes.map(lane => lane.id === sourceLane.id ? reorderedLane : lane) }
+    const reorderedLane = reorderCardsOnLane(sourceLane, cards => {
+      return changeElementOfPositionInArray(cards, source.index, destination.index)
+    })
+    reorderedBoard = reorderLanesOnBoard(board, lane => lane.id === sourceLane.id ? reorderedLane : lane)
   } else {
-    const reorderedSourceLane = { ...sourceLane, cards: removeFromArrayAtPosition(sourceLane.cards, source.index) }
-    const reorderedDestinationLane = { ...destinationLane, cards: addInArrayAtPosition(destinationLane.cards, sourceLane.cards[source.index], destination.index) }
-    reorderedBoard = { ...board,
-      lanes: board.lanes.map(lane => {
-        if (lane.id === sourceLane.id) return reorderedSourceLane
-        if (lane.id === destinationLane.id) return reorderedDestinationLane
-        return lane
-      })
-    }
+    const reorderedSourceLane = reorderCardsOnLane(sourceLane, cards => {
+      return removeFromArrayAtPosition(cards, source.index)
+    })
+    const reorderedDestinationLane = reorderCardsOnLane(destinationLane, cards => {
+      return addInArrayAtPosition(cards, sourceLane.cards[source.index], destination.index)
+    })
+    reorderedBoard = reorderLanesOnBoard(board, lane => {
+      if (lane.id === sourceLane.id) return reorderedSourceLane
+      if (lane.id === destinationLane.id) return reorderedDestinationLane
+      return lane
+    })
   }
 
   return reorderedBoard
