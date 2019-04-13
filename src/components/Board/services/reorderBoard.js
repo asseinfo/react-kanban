f51846518ba168d28547a774a631c3eee1631a5e
Leandro Lourenci <leandro@lourenci.com>
2019-04-13 09:04:19 -0300

    Fix codeclimate issue on reorderBoard function

--- src/components/Board/services/reorderBoard.js
+++ src/components/Board/services/reorderBoard.js
@@ -1,21 +1,16 @@
 import { removeFromArrayAtPosition, addInArrayAtPosition, changeElementOfPositionInArray } from '@services/utils'
 
 export default function reorderBoard (board, source, destination) {
-  return source.laneId ? reorderCardsOnBorder(board, source, destination) : reorderLaneOnBorder(board, source, destination)
+  const reorderFn = source.laneId ? reorderCardsOnBorder : reorderLaneOnBorder
+  return reorderFn(board, source, destination)
 }
 
 function reorderCardsOnBorder (board, source, destination) {
-  function reorderCardsOnLane (lane, reorderCards) {
-    return { ...lane, cards: reorderCards(lane.cards) }
-  }
-
-  function reorderLanesOnBoard (board, reorderLanesMapper) {
-    return { ...board, lanes: board.lanes.map(reorderLanesMapper) }
-  }
-
   const sourceLane = board.lanes.find(lane => lane.id === source.laneId)
   const destinationLane = board.lanes.find(lane => lane.id === destination.laneId)
-  const reorderLanes = reorderLanesOnBoard.bind(null, board)
+
+  const reorderLanesOnBoard = reorderLanesMapper => ({ ...board, lanes: board.lanes.map(reorderLanesMapper) })
+  const reorderCardsOnLane = (lane, reorderCards) => ({ ...lane, cards: reorderCards(lane.cards) })
   const reorderCardsOnSourceLane = reorderCardsOnLane.bind(null, sourceLane)
   const reorderCardsOnDestinationLane = reorderCardsOnLane.bind(null, destinationLane)
 
@@ -23,7 +18,7 @@ function reorderCardsOnBorder (board, source, destination) {
     const reorderedCardsOnLane = reorderCardsOnSourceLane(cards => {
       return changeElementOfPositionInArray(cards, source.index, destination.index)
     })
-    return reorderLanes(lane => lane.id === sourceLane.id ? reorderedCardsOnLane : lane)
+    return reorderLanesOnBoard(lane => lane.id === sourceLane.id ? reorderedCardsOnLane : lane)
   } else {
     const reorderedCardsOnSourceLane = reorderCardsOnSourceLane(cards => {
       return removeFromArrayAtPosition(cards, source.index)
@@ -31,7 +26,7 @@ function reorderCardsOnBorder (board, source, destination) {
     const reorderedCardsOnDestinationLane = reorderCardsOnDestinationLane(cards => {
       return addInArrayAtPosition(cards, sourceLane.cards[source.index], destination.index)
     })
-    return reorderLanes(lane => {
+    return reorderLanesOnBoard(lane => {
       if (lane.id === sourceLane.id) return reorderedCardsOnSourceLane
       if (lane.id === destinationLane.id) return reorderedCardsOnDestinationLane
       return lane
