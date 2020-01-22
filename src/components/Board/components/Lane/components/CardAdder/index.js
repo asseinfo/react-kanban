307d28fe26a52e0a1e668321f5cdcad80073be49
Matheus <matheusouzatj@gmail.com>
2020-01-21 21:24:56 -0300

    create addcard

--- src/components/Board/components/Lane/components/CardAdder/index.js
+++ src/components/Board/components/Lane/components/CardAdder/index.js
@@ -1,101 +1,37 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
-import CardSkeleton from '../../../CardSkeleton'
+import CardForm from './components/CardForm'
 
 const AddCardButton = styled.button`
   width: 100%;
   margin-top: 5px;
   background-color: transparent;
-  border: none;
   cursor: pointer;
   border: 1px solid #ccc;
+  transition: 0.3s;
   :hover {
-    transition: 0.3s;
     background-color: #ccc;
   }
+  border-radius: 3px;
   font-size: 20px;
   margin-bottom: 10px;
-`
-const DefaultCard = styled(CardSkeleton)`
-  border-radius: 3px;
-  background-color: #fff;
-  padding: 10px;
-  margin-bottom: 7px;
-  input {
-    border: 0px;
-    font-family: inherit;
-    font-size: inherit;
-  }
+  font-weight: bold;
 `
 
-const CardTitle = styled.div`
-  input{
-    font-weight: bold;
-    border-bottom: 1px solid #eee;
-    padding-bottom: 5px;
-    font-weight: bold;
-    display: flex;
-    justify-content: space-between;
-    width: 100%;
-    padding: 0px;
-  }
-
-`
-  const FormButtonsWrapper = styled.div`
-    display: flex;
-    justify-content: space-between;
-    margin-top: 5px;
-    button{
-      background-color: #eee;
-      border: 1px solid #ccc;
-      border: none;
-      padding: 5px;
-      width:80%
-      border-radius: 3px;
-      cursor: pointer;
-    }
-    button:hover {
-      transition: 0.3s;
-      background-color: #ccc;
-    }
-    button:first-of-type{
-      margin-right:15px;
-    }
-  `
-
-const CardDescription = styled.div`
-  input{
-    width:100%;
-  }
-  padding-top: 10px;
-`
-
-export default function CardAdder({ lane, onCardAdd }) {
-  function handleCardAdd(event) {
-    event.preventDefault()
-    onCardAdd(lane, { id: 4, title: 'Card adicionado', description: 'Testando' })
+export default function CardAdder({ lane, onConfirm }) {
+  function confirmCard(card) {
+    onConfirm(lane, card)
+    setAddingCard(false)
   }
 
   const [addingCard, setAddingCard] = useState(false)
 
   return (
     <>
-      <AddCardButton onClick={() => setAddingCard(!addingCard)}>+</AddCardButton>
-      {addingCard && (
-        <DefaultCard>
-          <span>
-            <CardTitle>
-              <input autoFocus defaultValue='Title'/>
-            </CardTitle>
-          </span>
-          <CardDescription>
-            <input defaultValue='Description' />
-          </CardDescription>
-          <FormButtonsWrapper>
-            <button >Save</button>
-            <button>Cancel</button>
-          </FormButtonsWrapper>
-        </DefaultCard>
+      {addingCard ? (
+        <CardForm onConfirm={confirmCard} onCancel={() => setAddingCard(false)} />
+      ) : (
+        <AddCardButton onClick={() => setAddingCard(!addingCard)}>+</AddCardButton>
       )}
     </>
   )
---
+++ src/components/Board/components/Lane/components/CardAdder/index.spec.js
@@ -0,0 +1,78 @@
+import React from 'react'
+import { render, fireEvent } from '@testing-library/react'
+import CardAdder from './'
+
+describe('<CardAdder />', () => {
+  let subject, onConfirm
+  const lane = { id: 1 }
+  function mount() {
+    onConfirm = jest.fn()
+
+    subject = render(<CardAdder lane={lane} onConfirm={onConfirm} />)
+  }
+
+  beforeEach(mount)
+  afterEach(() => {
+    subject = onConfirm = undefined
+  })
+
+  it('renders the button to add a new card', () => {
+    expect(subject.queryByText('+')).toBeInTheDocument()
+  })
+
+  describe('when the user clicks to add a new card', () => {
+    beforeEach(() => fireEvent.click(subject.queryByText('+')))
+
+    it('hides the card placeholder', () => {
+      expect(subject.queryByText('+')).not.toBeInTheDocument()
+    })
+
+    it('renders the card inputs', () => {
+      expect(subject.container.querySelector('input[name="title"]')).toBeInTheDocument()
+      expect(subject.container.querySelector('input[name="description"]')).toBeInTheDocument()
+    })
+
+    describe('when the user confirms the new card', () => {
+      beforeEach(() => {
+        fireEvent.change(subject.container.querySelector('input[name="title"]'), {
+          target: { value: 'Card Added by user' }
+        })
+        fireEvent.change(subject.container.querySelector('input[name="description"]'), {
+          target: { value: 'Description' }
+        })
+        fireEvent.click(subject.queryByText('Add'))
+      })
+
+      it('calls the "onConfirm" prop passing the new card and the lane', () => {
+        expect(onConfirm).toHaveBeenCalledTimes(1)
+        expect(onConfirm).toHaveBeenCalledWith(lane, { title: 'Card Added by user', description: 'Description' })
+      })
+
+      it('hides the input', () => {
+        expect(subject.container.querySelector('input')).not.toBeInTheDocument()
+      })
+
+      it('renders the placeholder to add a new card', () => {
+        expect(subject.queryByText('+')).toBeInTheDocument()
+      })
+    })
+
+    describe('when the user cancels the new card', () => {
+      beforeEach(() => {
+        fireEvent.click(subject.queryByText('Cancel'))
+      })
+
+      it('does not call the "onConfirm" prop', () => {
+        expect(onConfirm).not.toHaveBeenCalled()
+      })
+
+      it('hides the input', () => {
+        expect(subject.container.querySelector('input')).not.toBeInTheDocument()
+      })
+
+      it('renders the placeholder to add a new card', () => {
+        expect(subject.queryByText('+')).toBeInTheDocument()
+      })
+    })
+  })
+})
