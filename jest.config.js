7c6d1ebc56ed17421d5650a391263af24471bc02
Leandro Lourenci <leandro@lourenci.com>
2020-01-27 21:18:44 -0300

    Upgrade RTL to 9.4.0 (#217)

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   collectCoverageFrom: ['src/**', '!src/index.js'],
   coverageDirectory: 'coverage',
   testMatch: ['<rootDir>/src/**/*.spec.js'],
-  setupFilesAfterEnv: ['@testing-library/react/cleanup-after-each', '@testing-library/jest-dom/extend-expect'],
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
   moduleNameMapper: {
     '^@services/(.*)$': '<rootDir>/src/services/$1'
   }
