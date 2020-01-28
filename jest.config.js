cd756fb0f05dab57e615d49e39664c6bc5cbf1df
Leandro Lourenci <leandro@lourenci.com>
2020-01-27 21:13:15 -0300

    Upgrade Jest to 25 (#215)

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,9 @@
 module.exports = {
-  testEnvironment: 'jest-environment-jsdom-fifteen',
   collectCoverage: true,
-  collectCoverageFrom: [
-    'src/**',
-    '!src/index.js'
-  ],
+  collectCoverageFrom: ['src/**', '!src/index.js'],
   coverageDirectory: 'coverage',
   testMatch: ['<rootDir>/src/**/*.spec.js'],
-  setupFilesAfterEnv: [
-    '@testing-library/react/cleanup-after-each',
-    '@testing-library/jest-dom/extend-expect'
-  ],
+  setupFilesAfterEnv: ['@testing-library/react/cleanup-after-each', '@testing-library/jest-dom/extend-expect'],
   moduleNameMapper: {
     '^@services/(.*)$': '<rootDir>/src/services/$1'
   }
