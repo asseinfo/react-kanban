a05a10a1baae3896e579a5439cbb21d71f0bd274
Matheus <matheusouzatj@gmail.com>
2020-02-03 22:37:12 -0300

    remove unused code

--- src/components/Board/components/Lane/components/CardAdder/components/CardForm/index.js
+++ src/components/Board/components/Lane/components/CardAdder/components/CardForm/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import styled from 'styled-components'
 import { when } from '@services/utils'
 import CardSkeleton from '../../../../../CardSkeleton'
