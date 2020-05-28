6adef31a515b0f27a1e03aa64a97389f707ad13d
Poli <matheuspolicamilo@gmail.com>
2020-05-27 23:20:11 -0300

    Remove unnecessary code

--- assets/index.js
+++ assets/index.js
@@ -12,44 +12,44 @@ const board = {
         {
           id: '0206c8d7-4d48-4d97-b867-86fc2d21075d',
           title: 'Card title 1',
-          description: 'Card content'
+          description: 'Card content',
         },
         {
           id: 2,
           title: 'Card title 2',
-          description: 'Card content'
+          description: 'Card content',
         },
         {
           id: 3,
           title: 'Card title 3',
-          description: 'Card content'
+          description: 'Card content',
         },
         {
           id: 4,
           title: 'Card title 4',
-          description: 'Card content'
+          description: 'Card content',
         },
         {
           id: 5,
           title: 'Card title 5',
-          description: 'Card content'
+          description: 'Card content',
         },
         {
           id: 6,
           title: 'Card title 6',
-          description: 'Card content'
+          description: 'Card content',
         },
         {
           id: 7,
           title: 'Card title 7',
-          description: 'Card content'
+          description: 'Card content',
         },
         {
           id: 8,
           title: 'Card title 8',
-          description: 'Card content'
-        }
-      ]
+          description: 'Card content',
+        },
+      ],
     },
     {
       id: 2,
@@ -58,20 +58,11 @@ const board = {
         {
           id: 9,
           title: 'Card title 9',
-          description: 'Card content'
-        }
-      ]
-    }
-  ]
+          description: 'Card content',
+        },
+      ],
+    },
+  ],
 }
 
-render(
-  <Board
-    {...getUrlParams()}
-    onColumnRemove={console.log}
-    onColumnRename={console.log}
-    onCardRemove={console.log}
-    initialBoard={board}
-  />,
-  document.getElementById('app')
-)
+render(<Board {...getUrlParams()} initialBoard={board} />, document.getElementById('app'))
