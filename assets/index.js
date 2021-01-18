ab5326d35d4b06e8dbf172fbac9d2d3636a49eec
viganzeqiri <viganzeqirii@gmail.com>
2021-01-18 10:37:12 +0100

    Added virtualized column

--- assets/index.js
+++ assets/index.js
@@ -8,70 +8,36 @@ const board = {
     {
       id: '0206c8d7-4d48-4d97-b867-86fc2d21074d',
       title: 'Column Backlog',
-      cards: [
-        {
-          id: '0206c8d7-4d48-4d97-b867-86fc2d21075d',
-          title: 'Card title 1',
-          description: 'Card content',
-        },
-        {
-          id: 2,
-          title: 'Card title 2',
-          description: 'Card content',
-        },
-        {
-          id: 3,
-          title: 'Card title 3',
-          description: 'Card content',
-        },
-        {
-          id: 4,
-          title: 'Card title 4',
-          description: 'Card content',
-        },
-        {
-          id: 5,
-          title: 'Card title 5',
-          description: 'Card content',
-        },
-        {
-          id: 6,
-          title: 'Card title 6',
-          description: 'Card content',
-        },
-        {
-          id: 7,
-          title: 'Card title 7',
-          description: 'Card content',
-        },
-        {
-          id: 8,
-          title: 'Card title 8',
-          description: 'Card content',
-        },
-      ],
+      cards: new Array(400).fill().map((u, i) => ({
+        id: i + 100,
+        title: `Card title ${i + 100}`,
+        description: 'Card content',
+      })),
     },
     {
       id: 2,
       title: 'Column Doing',
-      cards: [
-        {
-          id: 9,
-          title: 'Card title 9',
-          description: 'Card content',
-        },
-      ],
+      cards: new Array(400).fill().map((u, i) => ({
+        id: i + 500,
+        title: `Card title ${i + 500}`,
+        description: 'Card content',
+      })),
+    },
+    {
+      id: 3,
+      title: 'Column Done',
+      cards: new Array(400).fill().map((u, i) => ({
+        id: i + 10000,
+        title: `Card title ${i + 10000}`,
+        description: 'Card content',
+      })),
     },
   ],
 }
 
 render(
-  <Board
-    {...getUrlParams()}
-    onColumnRemove={console.log}
-    onColumnRename={console.log}
-    onCardRemove={console.log}
-    initialBoard={board}
-  />,
+  <Board virtualLists {...getUrlParams()}>
+    {board}
+  </Board>,
   document.getElementById('app')
 )
