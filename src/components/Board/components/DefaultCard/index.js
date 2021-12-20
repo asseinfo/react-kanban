49cfa06fa36b09436bca4bded4f8fb98732917b2
Maciej <parfienczyk98@gmail.com>
2021-12-20 14:33:53 +0100

    Fixed disappearing content on clicking in edit icon

--- src/components/Board/components/DefaultCard/index.js
+++ src/components/Board/components/DefaultCard/index.js
@@ -28,7 +28,6 @@ export default function ({
         style={{ cursor: 'pointer' }}
         onClick={() => {
           setEdit(true)
-          onCardEdit(card)
         }}
       >
         {EditIcon()}
